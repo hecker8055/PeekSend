3ceef06a6666a7cc88cb5079b738944dc05849ce
hecker8055 <146121211+hecker8055@users.noreply.github.com>
2026-01-09 00:46:44 +0530

    Add Nhost functions

--- functions/update.js
+++ functions/update.js
@@ -1,18 +1,18 @@
 import { NhostClient } from "@nhost/nhost-js";
 
-const nhost = new NhostClient({
-  backendUrl: process.env.NHOST_BACKEND_URL,
-});
-
-nhost.graphql.setAccessToken(process.env.NHOST_ADMIN_SECRET);
-
 export default async (req, res) => {
-  const img_text = req.query.img_text;
-  const user = req.query.user;
+  console.log("Pixel hit:", req.query);
 
-  console.log("Pixel hit:", { img_text, user });
+  const imgText = req.query.email_id;
+  const userId = req.query.user_id;
 
-  if (img_text && user) {
+  const nhost = new NhostClient({
+    backendUrl: process.env.NHOST_BACKEND_URL,
+  });
+
+  nhost.graphql.setAccessToken(process.env.NHOST_ADMIN_SECRET);
+
+  if (imgText && userId) {
     const UPDATE_EMAIL = `
       mutation UpdateEmail($img_text: String!, $user: uuid!, $date: timestamptz!) {
         update_emails(
@@ -27,22 +27,14 @@ export default async (req, res) => {
       }
     `;
 
-    try {
-      const result = await nhost.graphql.request(UPDATE_EMAIL, {
-        img_text,
-        user,
-        date: new Date().toISOString(),
-      });
-
-      console.log("Update Result:", result);
-    } catch (error) {
-      console.error("GraphQL Error:", error);
-    }
-  } else {
-    console.log("Missing parameters:", req.query);
+    await nhost.graphql.request(UPDATE_EMAIL, {
+      img_text: imgText,
+      user: userId,
+      date: new Date().toISOString(),
+    });
   }
 
-  // return transparent pixel
+  // Always return GIF
   const gif = Buffer.from(
     "R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
     "base64"
@@ -50,6 +42,5 @@ export default async (req, res) => {
 
   res.setHeader("Content-Type", "image/gif");
   res.setHeader("Content-Length", gif.length);
-
-  return res.status(200).send(gif);
+  res.status(200).send(gif);
 };
